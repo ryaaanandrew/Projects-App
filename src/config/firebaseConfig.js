f0065af5ce9ca9b6c4652234ea1f8c2392f101d5
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-08-08 22:35:11 -0400

    added config file for firebase

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA9fBDa4HG45sHaHDH7r6yTljQPiJ89tC4",
+    authDomain: "projects-app-e83a4.firebaseapp.com",
+    databaseURL: "https://projects-app-e83a4.firebaseio.com",
+    projectId: "projects-app-e83a4",
+    storageBucket: "",
+    messagingSenderId: "1009868823614",
+    appId: "1:1009868823614:web:64f9d1b00034082a"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
